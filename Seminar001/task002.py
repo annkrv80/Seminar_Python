080c402d13d9f65376927d1d99d571f8e9791590
Anna <annkrv80@yandex.ru>
2022-09-26 00:12:59 +0700

    Нахождение максимального числа

---
+++ Seminar001/task002.py
@@ -0,0 +1,19 @@
+#Напишите программу, которая на вход принимает 5 чисел и находит максимальное из них.
+import os
+clear = lambda: os.system('cls')
+clear()
+a = int (input ('Введите число a:'))
+b = int (input ('Bведите число b:'))
+c = int (input ('Введите число с:'))
+d = int (input ('Bведите число d:'))
+e = int (input ('Bведите число e:'))
+max=a
+if b>max:
+   max=b
+if c>max:
+   max=c
+if d>max:
+   max=d
+if e>max:
+    max=e
+print(max)
