df9d0957350a00525dd1b270e1645654add71654
Anna <annkrv80@yandex.ru>
2022-09-28 23:13:04 +0700

    является ли одно числоквадратом другого

--- Seminar001/task002.py
+++ Seminar001/task002.py
@@ -2,11 +2,11 @@
 import os
 clear = lambda: os.system('cls')
 clear()
-a = int (input ('Введите число a:'))
-b = int (input ('Bведите число b:'))
-c = int (input ('Введите число с:'))
-d = int (input ('Bведите число d:'))
-e = int (input ('Bведите число e:'))
+a = int(input('Введите число a:'))
+b = int(input('Bведите число b:'))
+c = int(input ('Введите число с:'))
+d = int(input ('Bведите число d:'))
+e = int(input ('Bведите число e:'))
 max=a
 if b>max:
    max=b
