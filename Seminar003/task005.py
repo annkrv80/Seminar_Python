35bcd2d46f4fc036c1c6a76f02ffd0de31f69d10
Anna <annkrv80@yandex.ru>
2022-10-05 02:18:49 +0700

    поиск второго вхождения

--- Seminar003/task005.py
+++ Seminar003/task005.py
@@ -1,12 +1,19 @@
 #Напишите программу, которая определит позицию второго вхождения строки в списке либо сообщит, что её нет.
 
-lst = ['один', 'два', 'три', 'один', 'три', 'пять']
-a = 'один'
-count = 0
-for i in range(len(lst)):
-    if a == lst[i]:
-        count += 1
-    if count == 2:
-       print(i)
-if count == 0:
-    print('Строки нет')
+from itertools import count
+
+
+lst = ['one', 'two', 'onem', 'one', 'two', 'one']
+a = 'onem'
+def check_list(lst, a):
+    count = 0
+    for i in range(len(lst)):
+        if lst[i] == a:
+            count += 1
+            if count == 2:
+                return i
+    else:
+        return 'строки нет'
+
+
+print(f"ответ: {check_list(lst, a)}")
